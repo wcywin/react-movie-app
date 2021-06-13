19e784f1eb2b54103ae5052720f3be334540c8a7
Wojciech Cywinski <wcywin@gmail.com>
2021-06-14 00:38:16 +0200

    api url with https

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,7 @@
 import axios from 'axios'
 
-const apiKey = 'b9bd48a6';
-// const apiKey = process.env.REACT_APP_OMDB_API_KEY;
-const dataBaseURL = `http://www.omdbapi.com/?apikey=${apiKey}`;
+const apiKey = process.env.REACT_APP_OMDB_API_KEY;
+const dataBaseURL = `https://www.omdbapi.com/?apikey=${apiKey}`;
 
 const api = axios.create({
   baseURL: dataBaseURL,
